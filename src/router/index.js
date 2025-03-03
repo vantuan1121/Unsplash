71353debc15577f7c2e3e121947f01c7a9f32774
vantuan1121 <nguyenvantuan.mail.vn@gmail.com>
2025-03-03 13:59:29 +0700

    Cập nhật code

--- src/router/index.js
+++ src/router/index.js
@@ -9,19 +9,19 @@ import UnsplashView from '@/views/Menu/UnsplashView.vue'
 import WallpapersView from '@/views/Menu/WallpapersView.vue'
 import NatureView from '@/views/Menu/NatureView.vue'
 //
-// import RendersView from '@/views/Menu/RendersView.vue'
-// import TexturesView from '@/views/Menu/TexturesView.vue'
-// import ArchitectureView from '@/views/Menu/ArchitectureView.vue'
-// import TravelView from '@/views/Menu/TravelView.vue'
-// import FilmView from '@/views/Menu/FilmView.vue'
-// import StreetPhotographyView from '@/views/Menu/StreetPhotographyView.vue'
-// import PeopleView from '@/views/Menu/PeopleView.vue'
-// import AnimalsView from '@/views/Menu/AnimalsView.vue'
-// import ExperimentalView from '@/views/Menu/ExperimentalView.vue'
-// import FashionView from '@/views/Menu/FashionView.vue'
-// import FoodView from '@/views/Menu/FoodView.vue'
-// import SportsView from '@/views/Menu/SportsView.vue'
-// import HealthView from '@/views/Menu/HealthView.vue'
+import RendersView from '@/views/Menu/RendersView.vue'
+import TexturesView from '@/views/Menu/TexturesView.vue'
+import ArchitectureView from '@/views/Menu/ArchitectureView.vue'
+import TravelView from '@/views/Menu/TravelView.vue'
+import FilmView from '@/views/Menu/FilmView.vue'
+import StreetPhotographyView from '@/views/Menu/StreetPhotographyView.vue'
+import PeopleView from '@/views/Menu/PeopleView.vue'
+import AnimalsView from '@/views/Menu/AnimalsView.vue'
+import ExperimentalView from '@/views/Menu/ExperimentalView.vue'
+import FashionView from '@/views/Menu/FashionView.vue'
+import FoodView from '@/views/Menu/FoodView.vue'
+import SportsView from '@/views/Menu/SportsView.vue'
+import HealthView from '@/views/Menu/HealthView.vue'
 
 import UnsplashAPI from '@/components/UnsplashAPI.vue'
 
@@ -79,71 +79,71 @@ const router = createRouter({
       name: 'NatureView',
       component: NatureView,
     },
-    // {
-    //   path: '/RendersView',
-    //   name: 'RendersView',
-    //   component: RendersView,
-    // },
-    // {
-    //   path: '/TexturesView',
-    //   name: 'TexturesView',
-    //   component: TexturesView,
-    // },
-    // {
-    //   path: '/ArchitectureView',
-    //   name: 'ArchitectureView',
-    //   component: ArchitectureView,
-    // },
-    // {
-    //   path: '/TravelView',
-    //   name: 'TravelView',
-    //   component: TravelView,
-    // },
-    // {
-    //   path: '/FilmView',
-    //   name: 'FilmView',
-    //   component: FilmView,
-    // },
-    // {
-    //   path: '/StreetPhotographyView',
-    //   name: 'StreetPhotographyView',
-    //   component: StreetPhotographyView,
-    // },
-    // {
-    //   path: '/PeopleView',
-    //   name: 'PeopleView',
-    //   component: PeopleView,
-    // },
-    // {
-    //   path: '/AnimalsView',
-    //   name: 'AnimalsView',
-    //   component: AnimalsView,
-    // },
-    // {
-    //   path: '/ExperimentalView',
-    //   name: 'ExperimentalView',
-    //   component: ExperimentalView,
-    // },
-    // {
-    //   path: '/FashionView',
-    //   name: 'FashionView',
-    //   component: FashionView,
-    // },
-    // {
-    //   path: '/FoodView',
-    //   name: 'FoodView',
-    //   component: FoodView,
-    // },
-    // {
-    //   path: '/SportsView',
-    //   name: 'SportsView',
-    //   component: SportsView,
-    // },
-    // {
-    //   path: '/HealthView',
-    //   name: 'HealthView',
-    //   component: HealthView,
-    // },
+    {
+      path: '/RendersView',
+      name: 'RendersView',
+      component: RendersView,
+    },
+    {
+      path: '/TexturesView',
+      name: 'TexturesView',
+      component: TexturesView,
+    },
+    {
+      path: '/ArchitectureView',
+      name: 'ArchitectureView',
+      component: ArchitectureView,
+    },
+    {
+      path: '/TravelView',
+      name: 'TravelView',
+      component: TravelView,
+    },
+    {
+      path: '/FilmView',
+      name: 'FilmView',
+      component: FilmView,
+    },
+    {
+      path: '/StreetPhotographyView',
+      name: 'StreetPhotographyView',
+      component: StreetPhotographyView,
+    },
+    {
+      path: '/PeopleView',
+      name: 'PeopleView',
+      component: PeopleView,
+    },
+    {
+      path: '/AnimalsView',
+      name: 'AnimalsView',
+      component: AnimalsView,
+    },
+    {
+      path: '/ExperimentalView',
+      name: 'ExperimentalView',
+      component: ExperimentalView,
+    },
+    {
+      path: '/FashionView',
+      name: 'FashionView',
+      component: FashionView,
+    },
+    {
+      path: '/FoodView',
+      name: 'FoodView',
+      component: FoodView,
+    },
+    {
+      path: '/SportsView',
+      name: 'SportsView',
+      component: SportsView,
+    },
+    {
+      path: '/HealthView',
+      name: 'HealthView',
+      component: HealthView,
+    },
   ],
 })
 
